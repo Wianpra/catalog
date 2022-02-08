e09a6d429de89494d767b038e918c3721b5e484f
wico.andre <wico.andre@gmail.com>
2022-02-08 18:17:57 +0800

    livewire

---
+++ app/Http/Livewire/ProductCatalog.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ProductCatalog extends Component
+{
+    public function render()
+    {
+        return view('livewire.product-catalog');
+    }
+}
