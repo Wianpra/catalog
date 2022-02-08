cb8b8ad9b909f30d84330977be3488b405405bf9
wico.andre <wico.andre@gmail.com>
2022-02-08 14:43:53 +0800

    push

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,380 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+ 
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Beck - Furniture eCommerce Bootstrap 5 Template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="assets/img/icon.png">
+
+    <!-- ************************* CSS Files ************************* -->
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+
+    <!-- Vendor CSS -->
+    <link rel="stylesheet" href="assets/css/vendor.css">
+
+    <!-- style css -->
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+
+<body>
+
+    <!-- Preloader Start -->
+    <div class="ft-preloader active">
+        <div class="ft-preloader-inner h-100 d-flex align-items-center justify-content-center">
+            <div class="ft-child ft-bounce1"></div>
+            <div class="ft-child ft-bounce2"></div>
+            <div class="ft-child ft-bounce3"></div>
+        </div>
+    </div>
+    <!-- Preloader End -->
+ 
+    <!-- Main Wrapper Start -->
+    <div class="wrapper">
+        @include('sweetalert::alert')
+        <!-- Header Start -->
+        @yield('content')
+        <!-- Main Content Wrapper End -->
+
+        <!-- Footer Start-->
+        <footer class="footer bg-color" data-bg-color="#f4f8fa">
+            <div class="footer-top">
+                <div class="container-fluid">
+                    <div class="row border-bottom pt--70 pb--70">
+                        <div class="col-lg-3 col-sm-6 offset-md-1 offset-lg-0 mb-md--45">
+                            <div class="footer-widget">
+                                <div class="textwidget">
+                                    <figure class="footer-logo mb--30">
+                                        <img src="assets/img/logo/logo.png" alt="Logo">
+                                    </figure>
+                                    <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms. </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-sm-3 offset-lg-1 offset-sm-2 mb-md--45">
+                            <div class="footer-widget">
+                                <h3 class="widget-title mb--35 mb-sm--20">Company</h3>
+                                <div class="footer-widget">
+                                    <ul class="footer-menu">
+                                        <li><a href="index.html">About Us</a></li>
+                                        <li><a href="blog.html">Blogs</a></li>
+                                        <li><a href="#">Careers</a></li>
+                                        <li><a href="contact-us.html">Contact</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-sm-4 offset-md-1 offset-lg-0 mb-xs--45">
+                            <div class="footer-widget">
+                                <h3 class="widget-title mb--35 mb-sm--20">Product</h3>
+                                <div class="footer-widget">
+                                    <ul class="footer-menu">
+                                        <li><a href="#">Pricing</a></li>
+                                        <li><a href="#">Features</a></li>
+                                        <li><a href="#">Customers</a></li>
+                                        <li><a href="#">Demos</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-sm-4 mb-xs--45">
+                            <div class="footer-widget">
+                                <h3 class="widget-title mb--35 mb-sm--20">Helps</h3>
+                                <div class="footer-widget">
+                                    <ul class="footer-menu">
+                                        <li><a href="#">Introduction</a></li>
+                                        <li><a href="#">Feedback</a></li>
+                                        <li><a href="#">Referals</a></li>
+                                        <li><a href="#">Network Status</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-md-3 col-sm-4">
+                            <div class="footer-widget">
+                                <h3 class="widget-title mb--35 mb-sm--20">Social Network</h3>
+                                <div class="footer-widget">
+                                    <ul class="footer-menu">
+                                        <li><a href="#">Facebook</a></li>
+                                        <li><a href="#">Twitter</a></li>
+                                        <li><a href="#">Linkedin</a></li>
+                                        <li><a href="#">Google +</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-middle ptb--40">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-11">
+                            <div class="footer-widget">
+                                <div class="taglist">
+                                    <span>Tags:</span>
+                                    <a href="#">Minimal eCommerce</a>
+                                    <a href="#">Marketing</a>
+                                    <a href="#">User Exprience</a>
+                                    <a href="#">Ali Express</a>
+                                    <a href="#">Web</a>
+                                    <a href="#">Digital Expo</a>
+                                    <a href="#">Web Search</a>
+                                    <a href="#">Affiliate</a>
+                                    <a href="#">UCWeb</a>
+                                    <a href="#">Support</a>
+                                    <a href="#">Template</a>
+                                    <a href="#">Best Queality</a>
+                                    <a href="#">Mobile</a>
+                                    <a href="#">24 Support</a>
+                                    <a href="#">Ali Express</a>
+                                    <a href="#">Web</a>
+                                    <a href="#">Laptop</a>
+                                    <a href="#">Web Search</a>
+                                    <a href="#">Affiliate</a>
+                                    <a href="#">Photoshop</a>
+                                    <a href="#">Support</a>
+                                    <a href="#">Template</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <div class="container-fluid">
+                    <div class="row border-top ptb--20">
+                        <div class="col-12 text-center">
+                            <p class="copyright-text">&copy; Beck 2021 Made With <i class="la la-heart"></i> By Hasthemes</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Footer End-->
+
+        <!-- OffCanvas Menu Start -->
+        <div class="offcanvas-menu-wrapper" id="offcanvasMenu">
+            <div class="offcanvas-menu-inner">
+                <a href="" class="btn-close">
+                    <i class="la la-remove"></i>
+                </a>
+                <nav class="offcanvas-navigation">
+                    <ul class="offcanvas-menu">
+                        <li class="menu-item-has-children active">
+                            <a href="#">Home</a>
+                            <ul class="sub-menu">
+                                <li>
+                                    <a href="index.html">Homepage 01</a>
+                                </li>
+                                <li>
+                                    <a href="index-02.html">Homepage 02</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="menu-item-has-children">
+                            <a href="shop.html">Shop</a>
+                            <ul class="sub-menu">
+                                <li class="menu-item-has-children">
+                                    <a href="#">
+                                        <span class="mm-text">Shop Grid</span>
+                                    </a>
+                                    <ul class="sub-menu">
+                                        <li>
+                                            <a href="shop-fullwidth.html">Full Width</a>
+                                        </li>
+                                        <li>
+                                            <a href="shop.html">Left Sidebar</a>
+                                        </li>
+                                        <li>
+                                            <a href="shop-right-sidebar.html">Right Sidebar</a>
+                                        </li>
+                                        <li>
+                                            <a href="shop-three-columns.html">Three Columns</a>
+                                        </li>
+                                        <li>
+                                            <a href="shop-four-columns.html">Four Columns</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="#">Shop List</a>
+                                    <ul class="sub-menu">
+                                        <li>
+                                            <a href="shop-list.html">Full Width</a>
+                                        </li>
+                                        <li>
+                                            <a href="shop-list-sidebar.html">Left Sidebar</a>
+                                        </li>
+                                        <li>
+                                            <a href="shop-list-right-sidebar.html">Right Sidebar</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="#">Product Details</a>
+                                    <ul class="sub-menu">
+                                        <li>
+                                            <a href="product-details.html">Tab Style 1</a>
+                                        </li>
+                                        <li>
+                                            <a href="product-details-tab-style-2.html">Tab Style 2</a>
+                                        </li>
+                                        <li>
+                                            <a href="product-details-tab-style-3.html">Tab Style 3</a>
+                                        </li>
+                                        <li>
+                                            <a href="product-details-gallery-left.html">Gallery Left</a>
+                                        </li>
+                                        <li>
+                                            <a href="product-details-gallery-right.html">Gallery Right</a>
+                                        </li>
+                                        <li>
+                                            <a href="product-details-sticky-left.html">Sticky Left</a>
+                                        </li>
+                                        <li>
+                                            <a href="product-details-sticky-right.html">Sticky Right</a>
+                                        </li>
+                                        <li>
+                                            <a href="product-details-slider-box.html">Slider Box</a>
+                                        </li>
+                                        <li>
+                                            <a href="product-details-slider-full-width.html">Slider Box Full Width</a>
+                                        </li>
+                                        <li>
+                                            <a href="product-details-affiliate.html">Affiliate Proudct</a>
+                                        </li>                                                    
+                                        <li>
+                                            <a href="product-details-variable.html">Variable Proudct</a>
+                                        </li>
+                                        <li>
+                                            <a href="product-details-group.html">Group Product</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="menu-item-has-children">
+                            <a href="blog.html">Blog</a>
+                            <ul class="sub-menu">
+                                <li class="menu-item-has-children">
+                                    <a href="#">Blog</a>
+                                    <ul class="sub-menu">
+                                        <li>
+                                            <a href="blog-left-sidebar.html">Blog Left Sidebar</a>
+                                        </li>
+                                        <li>
+                                            <a href="blog.html">Blog Right Sidebar</a>
+                                        </li>
+                                        <li>
+                                            <a href="blog-01-column.html">Blog 01 column</a>
+                                        </li>
+                                        <li>
+                                            <a href="blog-02-columns.html">Blog 02 columns</a>
+                                        </li>
+                                        <li>
+                                            <a href="blog-03-columns.html">Blog 03 columns</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="#">Blog Details</a>
+                                    <ul class="sub-menu">
+                                        <li>
+                                            <a href="blog-details-audio.html">Audio Blog Details</a>
+                                        </li>
+                                        <li>
+                                            <a href="blog-details-gallery.html">Gallery Blog Details</a>
+                                        </li>
+                                        <li>
+                                            <a href="blog-details-image.html">image Blog Details</a>
+                                        </li>
+                                        <li>
+                                            <a href="blog-details-video.html">Video Blog Details</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="menu-item-has-children">
+                            <a href="#">Pages</a>
+                            <ul class="sub-menu">
+                                <li>
+                                    <a href="my-account.html">My Account</a>
+                                </li>
+                                <li>
+                                    <a href="checkout.html">Checkout</a>
+                                </li>
+                                <li>
+                                    <a href="cart.html">Cart</a>
+                                </li>
+                                <li>
+                                    <a href="compare.html">Compare</a>
+                                </li>
+                                <li>
+                                    <a href="order-tracking.html">Track Order</a>
+                                </li>
+                                <li>
+                                    <a href="wishlist.html">Wishlist</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="contact-us.html">Contact Us</a>
+                        </li>
+                    </ul>
+                    <div class="site-info vertical">
+                        <div class="site-info__item">
+                            <a href="tel:+01223566678"><strong>+01 2235 666 78</strong></a>
+                            <a href="mailto:Support@contixs.com">Support@furtrate.com</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <!-- OffCanvas Menu End -->
+
+        
+
+        <!-- Searchform Popup Start -->
+        <div class="searchform__popup" id="searchForm">
+            <a href="#" class="btn-close"><i class="la la-remove"></i></a>
+            <div class="searchform__body">
+                <p>Start typing and press Enter to search</p>
+                <form class="searchform">
+                    <input type="text" name="popup-search" id="popup-search" class="searchform__input" placeholder="Search Entire Store...">
+                    <button type="submit" class="searchform__submit"><i class="la la-search"></i></button>
+                </form>
+            </div>
+        </div>
+        <!-- Searchform Popup End -->
+
+        
+
+        <!-- Global Overlay Start -->
+        <div class="global-overlay"></div>
+        <!-- Global Overlay End -->
+
+        <!-- Global Overlay Start -->
+        <a class="scroll-to-top" href=""><i class="la la-angle-double-up"></i></a>
+        <!-- Global Overlay End -->
+    </div>
+    <!-- Main Wrapper End -->
+ 
+
+    <!-- ************************* JS Files ************************* -->
+
+    <!-- jQuery JS -->
+    <script src="assets/js/vendor.js"></script>
+
+    <!-- Main JS -->
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
