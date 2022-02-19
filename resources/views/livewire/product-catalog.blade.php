2160a54059a1751e77c19d4ba148a002f28ec09f
Andi Alfirdaus <andisr131117@gmail.com>
2022-02-19 14:32:54 +0800

    user product

--- resources/views/livewire/product-catalog.blade.php
+++ resources/views/livewire/product-catalog.blade.php
@@ -11,12 +11,9 @@
                                     <div class="shop-toolbar__left">
                                         <div class="product-ordering">
                                             <select class="product-ordering__select nice-select">
-                                                <option value="0">Default Sorting</option>
-                                                <option value="1">Relevance</option>
-                                                <option value="2">Name, A to Z</option>
-                                                <option value="3">Name, Z to A</option>
-                                                <option value="4">Price, low to high</option>
-                                                <option value="5">Price, high to low</option>
+                                                <option value="0">Relevance</option>
+                                                <option value="1">Name, A to Z</option>
+                                                <option value="2">Name, Z to A</option>
                                             </select>
                                         </div>
                                     </div>
@@ -45,12 +42,12 @@
                                             <div class="product-image">
                                                 <figure class="product-image--holder">
                                                     @php
-                                                        $img = unserialize($item->img);
+                                                    $img = unserialize($item->img);
                                                     @endphp
                                                     @if ( $img == null )
-                                                        <img src="assets/img/products/prod-04-270x300.jpg" alt="Products">
+                                                    <img src="assets/img/products/prod-04-270x300.jpg" class="img-thumbnail" alt="Products">
                                                     @else
-                                                        <img src="{{ asset('images/'.$img[0]) }}" alt="Product">
+                                                    <img src="{{ asset('images/'.$img[0]) }}" alt="Product" class="img-thumbnail">
                                                     @endif
                                                 </figure>
                                                 <a href="product-details.html" class="product-overlay"></a>
@@ -58,35 +55,40 @@
                                                     <a data-bs-toggle="modal" data-bs-target="#productModal" class="action-btn">
                                                         <i class="la la-eye"></i>
                                                     </a>
+                                                    @if ($item->seen = null)
+                                                    0
+                                                    @else
+                                                    {{ $item->seen }}
+                                                    @endif
                                                 </div>
                                             </div>
                                             <div class="product-info">
                                                 <div class="product-category">
-                                                    <a href="product-details.html">
+                                                    <a style="text-transform: capitalize" href="product-details.html">
                                                         @foreach ($category as $data)
-                                                            @if ( $item->category == $data->id)
-                                                                {{ $data->category }}
-                                                            @endif
+                                                        @if ( $item->category == $data->id)
+                                                        {{ $data->category }}
+                                                        @endif
                                                         @endforeach
                                                         
                                                     </a>
                                                 </div>
-                                                <h3 class="product-title"><a href="product-details.html">{{ $item->name }}</a></h3>
+                                                <h3 class="product-title"><a style="text-transform: capitalize" href="product-details.html">{{ $item->name }}</a></h3>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="ft-product-list">
                                         <div class="product-inner">
-                                            <figure class="product-image">
+                                            <figure class="product-image img-thumbnail">
                                                 <a href="product-details.html">
                                                     
                                                     @php
-                                                        $img = unserialize($item->img);
+                                                    $img = unserialize($item->img);
                                                     @endphp
                                                     @if ( $img == null )
-                                                        <img src="assets/img/products/prod-04-270x300.jpg" alt="Products">
+                                                    <img src="assets/img/products/prod-04-270x300.jpg" class="img-thumbnail mx-auto d-block" alt="Products">
                                                     @else
-                                                        <img src="{{ asset('images/'.$img[0]) }}" class="img-responsive" alt="Product">
+                                                    <img src="{{ asset('images/'.$img[0]) }}" style="height: 300px; object-fit: contain;" class="mx-auto d-block" alt="Product">
                                                     @endif
                                                 </a>
                                                 <div class="product-thumbnail-action">
@@ -94,6 +96,11 @@
                                                         <i class="la la-eye"></i>
                                                     </a>
                                                 </div>
+                                                @if ($item->seen = null)
+                                                0
+                                                @else
+                                                {{ $item->seen }}
+                                                @endif
                                             </figure>
                                             <div class="product-info">
                                                 <h3 class="product-title mb--25">
@@ -134,13 +141,19 @@
                             <div class="shop-widget mb--40">
                                 <h3 class="widget-title mb--25">Category</h3>
                                 <ul class="widget-list category-list">
+                                    <li>
+                                        <a wire:click.prevent="filterCategory(null)" role="button">
+                                            <span style="text-transform: capitalize" class="category-title">All</span>
+                                            <i class="fa fa-angle-double-right"></i>
+                                        </a>
+                                    </li>
                                     @foreach ($category as $data)
-                                        <li>
-                                            <a wire:click.prevent="filterCategory({{ $data->id }})" role="button">
-                                                <span class="category-title">{{ $data->category }}</span>
-                                                <i class="fa fa-angle-double-right"></i>
-                                            </a>
-                                        </li>
+                                    <li>
+                                        <a wire:click.prevent="filterCategory({{ $data->id }})" role="button">
+                                            <span style="text-transform: capitalize" class="category-title">{{ $data->category }}</span>
+                                            <i class="fa fa-angle-double-right"></i>
+                                        </a>
+                                    </li>
                                     @endforeach
                                 </ul>
                             </div>
